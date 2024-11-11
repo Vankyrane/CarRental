b428ec4eaf4d804842654d6c8f5b385f55fd4fb4
Prasad <prasaddrane@gmail.com>
2024-11-11 16:39:04 +0000

    Adding Car Model class

---
+++ src/main/java/com/car_rental/project/model/Car.java
@@ -0,0 +1,30 @@
+package com.car_rental.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, message = "Category name cannot be less than 3 characters")
+    private String categoryName;
+
+    private boolean isAvailable = true;
+
+}
+
