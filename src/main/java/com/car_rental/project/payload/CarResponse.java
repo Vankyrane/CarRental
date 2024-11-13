b0b7864b1245dc837a773de252b45fe01415ea25
Prasad <prasaddrane@gmail.com>
2024-11-13 17:54:01 +0000

    Adding Model Mapper and DTO's

---
+++ src/main/java/com/car_rental/project/payload/CarResponse.java
@@ -0,0 +1,14 @@
+package com.car_rental.project.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarResponse {
+    List<CarDTO> content;
+}
