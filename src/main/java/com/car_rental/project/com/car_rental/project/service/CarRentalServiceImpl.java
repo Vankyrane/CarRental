7426b3e4e69cd16dac80f9c8aac860ec32f1f7de
Vankyrane <78443716+Vankyrane@users.noreply.github.com>
2024-11-13 23:09:24 +0000

    Adding Exception Handling Logic for custome exceptions (#15)

--- src/main/java/com/car_rental/project/com/car_rental/project/service/CarRentalServiceImpl.java
+++ src/main/java/com/car_rental/project/com/car_rental/project/service/CarRentalServiceImpl.java
@@ -1,5 +1,7 @@
 package com.car_rental.project.com.car_rental.project.service;
 
+import com.car_rental.project.exception.APIException;
+import com.car_rental.project.exception.ResourceNotFoundException;
 import com.car_rental.project.model.Car;
 import com.car_rental.project.model.CarType;
 import com.car_rental.project.model.Reservation;
@@ -43,7 +45,7 @@ public class CarRentalServiceImpl implements CarRentalService {
             reservation.setEndDate(startDate.plusDays(numberOfDays));
             return reservationRepository.save(reservation);
         }else{
-            throw  new RuntimeException("No cars available for the selected car type");
+            throw  new ResourceNotFoundException("Car", "carType", String.valueOf(carType));
         }
     }
 
@@ -52,7 +54,7 @@ public class CarRentalServiceImpl implements CarRentalService {
         Car car = modelMapper.map(carDTO,Car.class);
         Car carFromDB = carRepository.findFirstByTypeAndIsAvailable(carDTO.getType(), carDTO.isAvailable());
         if(carFromDB != null){
-            throw new RuntimeException("Car with Type "+  car.getType() + " aleardy Exist");
+            throw new APIException("Car with Type "+  car.getType() + " aleardy Exist");
         }
         Car savedCar = carRepository.save(car);
         return modelMapper.map(savedCar, CarDTO.class);
@@ -61,6 +63,9 @@ public class CarRentalServiceImpl implements CarRentalService {
    @Override
    public List<CarDTO> getAllCars(){
         List<Car> carList = carRepository.findAll();
+        if(carList.isEmpty()){
+            throw new APIException("There are no Cars in Garage till now");
+        }
         List<CarDTO> carDTOList = carList.stream().map(car -> modelMapper.map(car,CarDTO.class)).toList();
         return carDTOList;
     }
@@ -68,7 +73,7 @@ public class CarRentalServiceImpl implements CarRentalService {
     @Override
     public CarDTO returnCar(Long reservationId){
         Reservation reservation = reservationRepository.findById(reservationId)
-                .orElseThrow(() -> new RuntimeException("Reservation not Found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Reservation", "reservationId", reservationId));
 
         Car car = reservation.getCar();
         car.setAvailable(true);
